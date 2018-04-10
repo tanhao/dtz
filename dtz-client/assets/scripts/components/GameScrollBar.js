87417cf37526fcfd35c57ed887d7ac437b39d748
tanhao <tanhao1210@gmail.com>
2018-04-11 01:39:07 +0800

    做游戏选择控件

--- dtz-client/assets/scripts/components/GameScrollBar.js
+++ dtz-client/assets/scripts/components/GameScrollBar.js
@@ -14,17 +14,28 @@ cc.Class({
     },
 
 
-    onGameBarScroll:function(scrollview, eventType, customEventData){
-        var posY=parseInt(scrollview.getContentPosition().y);
+    onGameBarScroll:function(scrollView, eventType, customEventData){
+        var posY=parseInt(scrollView.getContentPosition().y);
         if(eventType=== cc.ScrollView.EventType.SCROLLING&&Math.abs(posY-this._lastPosY)>1){
             this._lastPosY=posY;
-            //console.log("SCROLLING",scrollview.isScrolling(),scrollview.isAutoScrolling(),scrollview.getContentPosition());
-            //cc.log(scrollview.getScrollOffset(),scrollview.getMaxScrollOffset());
-            cc.log(posY,parseInt(posY/200),scrollview.node.getComponents(cc.Button).length);
-            var items=scrollview.getComponentsInChildren(cc.Sprite);
+            var btnHeight=174;
+            var currentIndex=parseInt((posY+(btnHeight/2)*(posY>0?1:-1))/btnHeight)+3;
+            var buttons=scrollView.getComponentsInChildren(cc.Button);
+            for(var i=0;i<buttons.length;i++){
+                var scale=Math.abs(posY+(3-i)*btnHeight)/800;
+                buttons[i].node.setScale(1-scale);
+                buttons[i].node.setColor(i==currentIndex?cc.color(255,255,255):cc.color(120,120,120));
+            }
         }else if(eventType=== cc.ScrollView.EventType.SCROLL_ENDED){
             console.log("SCROLL_ENDED");
+           
+        }else if(eventType=== cc.ScrollView.EventType.AUTOSCROLL_ENDED_WITH_THRESHOLD){
+            console.log("AUTOSCROLL_ENDED_WITH_THRESHOLD");
+            var max=scrollView.getMaxScrollOffset().y;
+            console.log("Y:",max)
+            scrollView.scrollToOffset(cc.p(max / 2, 0), 0.5);
         }
+        
     }
 
 });
